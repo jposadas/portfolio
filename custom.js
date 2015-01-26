a68dab5808f9ffd48e20e9de859b5bad0ce14d84
Juan Posadas <jposadas@stanford.edu>
2015-01-26 07:11:41 -0800

    final changes

--- custom.js
+++ custom.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('#portfolio').show();
 
     $('.page-link').click(function(event) {
-        event.preventDefault();
         var pageID = $(this).attr('data-page');
         $('.page-link').removeClass('green');
         $(this).addClass('green');
@@ -12,11 +11,34 @@ $(document).ready(function() {
 
     });
 
+    var pictureMode = false;
+
     $('.main-content .link').hover(function(event) {
         var src = $(this).attr('data-src');
         $('.main-content').css('background-image', 'url("' + src+ '")');
-    }, function() {
-        $('.main-content').css('background-image', 'none');
+        $('.main-content #about').css('opacity', '0');
+        pictureMode = true;
+
+     });
+
+    $('.main-content').mousemove(function(event) {
+        event.stopPropagation();
+        if (pictureMode && $(event.target).hasClass('link') == false) {
+            // console.log(event.target);
+            console.log('movin');
+            $('.main-content').css('background-image', 'none');
+            $('.main-content #about').css('opacity', '1.0');
+            pictureMode = false;
+        } else {
+            console.log('Whats good');
+        }
     });
+
+    
+
+     //, function() {
+    //     $('.main-content').css('background-image', 'none');
+    //     $('#about').show();
+    // });
 
 });
